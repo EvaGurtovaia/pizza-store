ad64e560eaba36d3bff32e5db926b8b1f4e8b987
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-06-08 19:03:48 -0500

    feat: add header and categories

---
+++ src/Categories.js
@@ -0,0 +1,18 @@
+import "./scss/app.scss";
+
+const Categories = () => {
+    return (
+        <div class="categories">
+            <ul>
+                <li class="active">All pizzas</li>
+                <li>Meat</li>
+                <li>Veggie</li>
+                <li>Seafood</li>
+                <li>Spicy</li>
+                <li>Stuffed</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Categories;
