b7c4fa322d78318d2a5a151699e396a4097c22c0
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-06-11 14:28:18 -0500

    feat: implement dynamically  rendering for categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -7,45 +7,27 @@ const Categories = () => {
         setActiveCategory(idx);
     };
 
+    const listOfCategories = [
+        "All Pizzas",
+        "Meat",
+        "Veggie",
+        "Seafood",
+        "Spicy",
+        "Stuffed",
+    ];
+
     return (
         <div className="categories">
             <ul>
-                <li
-                    onClick={() => onClickCategory(0)}
-                    className={activeCategory === 0 ? "active" : " "}
-                >
-                    All pizzas
-                </li>
-                <li
-                    onClick={() => onClickCategory(1)}
-                    className={activeCategory === 1 ? "active" : " "}
-                >
-                    Meat
-                </li>
-                <li
-                    onClick={() => onClickCategory(2)}
-                    className={activeCategory === 2 ? "active" : " "}
-                >
-                    Veggie
-                </li>
-                <li
-                    onClick={() => onClickCategory(3)}
-                    className={activeCategory === 3 ? "active" : " "}
-                >
-                    Seafood
-                </li>
-                <li
-                    onClick={() => onClickCategory(4)}
-                    className={activeCategory === 4 ? "active" : " "}
-                >
-                    Spicy
-                </li>
-                <li
-                    onClick={() => onClickCategory(5)}
-                    className={activeCategory === 5 ? "active" : " "}
-                >
-                    Stuffed
-                </li>
+                {listOfCategories.map((category, idx) => (
+                    <li
+                        onClick={() => onClickCategory(idx)}
+                        className={activeCategory === idx ? "active" : " "}
+                    >
+                        {category}
+                    </li>
+                ))}
+                ;
             </ul>
         </div>
     );
