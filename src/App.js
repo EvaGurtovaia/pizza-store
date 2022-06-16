b6d74a34d45504cb6dbd33023c1df069a2c8f258
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-06-16 14:46:02 -0500

    feat: add home and not found pages

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
-import { useState, useEffect } from "react";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaItem from "./components/PizzaItem";
-import Skeleton from "./components/Skeleton";
+import Home from "./pages/Home";
 import "./scss/app.scss";
 import React from "react";
 
 const App = (props) => {
-    const [items, setItems] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        fetch(`https://62aa13613b3143855441902b.mockapi.io/items`)
-            .then((res) => {
-                return res.json();
-            })
-            .then((arr) => {
-                setItems(arr);
-                setIsLoading(false);
-            });
-    }, []);
     return (
         <div>
             <div className="wrapper">
                 <Header />
                 <div className="content">
                     <div className="container">
-                        <div className="content__top">
-                            <Categories />
-                            <Sort />
-                        </div>
-                        <div>
-                            <h2 className="content__title">All pizzas</h2>
-                            <div className="content__items">
-                                {isLoading
-                                    ? [...new Array(6)].map((_, idx) => (
-                                          <Skeleton key={idx} />
-                                      ))
-                                    : items.map((pizza) => (
-                                          <PizzaItem
-                                              key={pizza.key}
-                                              title={pizza.title}
-                                              price={pizza.price}
-                                              image={pizza.imageUrl}
-                                              sizes={pizza.sizes}
-                                              types={pizza.types}
-                                          />
-                                      ))}
-                            </div>
-                        </div>
+                        <Home />
                     </div>
                 </div>
             </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/Skeleton";
+import PizzaItem from "../components/PizzaItem";
+
+const Home = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`https://62aa13613b3143855441902b.mockapi.io/items`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((arr) => {
+                setItems(arr);
+                setIsLoading(false);
+            });
+    }, []);
+    return (
+        <>
+            <div className="content__top">
+                <Categories />
+                <Sort />
+            </div>
+            <div>
+                <h2 className="content__title">All pizzas</h2>
+                <div className="content__items">
+                    {isLoading
+                        ? [...new Array(6)].map((_, idx) => (
+                              <Skeleton key={idx} />
+                          ))
+                        : items.map((pizza) => (
+                              <PizzaItem
+                                  key={pizza.key}
+                                  title={pizza.title}
+                                  price={pizza.price}
+                                  image={pizza.imageUrl}
+                                  sizes={pizza.sizes}
+                                  types={pizza.types}
+                              />
+                          ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Home;
